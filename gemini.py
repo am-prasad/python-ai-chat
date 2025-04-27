1fc4127eabea7ea561ad8b86c8ddaf908d12b09d
am-prasad <amprasad18@gmail.com>
2025-04-27 09:24:04 +0530

    ai chat bot using gemini-2.0-flash

---
+++ gemini.py
@@ -0,0 +1,13 @@
+import google.generativeai as genai
+
+API_KEY="AIzaSyBxWIWCGfP12Y0DKWq2adEXybF8fQgJ2JQ"
+genai.configure(api_key=API_KEY)
+model=genai.GenerativeModel("gemini-2.0-flash")
+chat=model.start_chat()
+print("Chat with Gemini! Type 'exit' to quit.")
+while True:
+    user_input=input("You:")
+    if user_input.lower=='exit':
+        break
+    response=chat.send_message(user_input)
+    print("Gemini:",response.text)
